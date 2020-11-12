4e9251098c292b55f29df82ea6ce5ae5dafc2c83
Suhayla924 <56284568+Suhayla924@users.noreply.github.com>
2020-11-11 21:10:45 -0800

    Add files via upload

---
+++ buildingAGame.2/player.js
@@ -0,0 +1,38 @@
+class Player {
+  constructor() {
+    this.r = 60;
+    this.x = w / 2;
+    this.y = h - this.r;
+    this.speed = 2;
+    this.direction = 'still';
+  }
+
+  display() {
+    // rect(this.x, this.y, this.r, this.r);
+    image(playerImg.x, playerImg.y, playerImg.r, playerImg.r);
+  }
+  move() {
+    switch (this.direction) {
+      case 'still':
+        //dont move anything
+        break;
+      case 'up':
+        // decrease y pos
+        this.y -= this.speed;
+        break;
+      case 'down':
+        //increase y position
+      this.y +=this.speed;
+        break;
+      case 'left':
+        //increase x pos
+        this.x -= this.speed;
+        break;
+      case 'right':
+        //decrease x position
+        this.x += this.speed;
+        break;
+    }
+  }
+
+}
