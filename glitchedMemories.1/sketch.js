155fba641f8ab56899df288c5312f1275fdbb37c
Suhayla924 <suhaylabhassan@gmail.com>
2020-12-01 16:20:12 -0800

    files

--- glitchedMemories.1/sketch.js
+++ glitchedMemories.1/sketch.js
@@ -2,14 +2,11 @@
 let player;
 let playerImg;
 let bgImg;
-let banImg;
-let bans = [];
 
 
 function preload() {
   playerImg = loadImage('assets/blue.png');
   bgImg = loadImage('assets/sky.JPG');
-  banImg = loadImage('assets/banana.png');
 }
 
 function setup() {
@@ -25,18 +22,11 @@ function keyPressed() {
 }
 
 function draw() {
-  if (random(1) < 0.01) {
-    bans.push(new Ban());
-  }
-
   background(bgImg);
+  
   player.show();
   player.move();
 
-   for (let b of bans){
-     b.move();
-     b.show();
-   }
 
 
 }
