b255e7172d87eba3ba0166c9eb1c234861b38fd4
Suhayla924 <56284568+Suhayla924@users.noreply.github.com>
2020-12-11 01:27:36 -0800

    files

---
+++ finalGame/coins.js
@@ -0,0 +1,17 @@
+class Coin{
+  constructor() {
+    this.r = 50;
+    this.x = 0 - this.r;
+    this.y = random(h);
+    this.speed = 4;
+  }
+
+  display() {
+    //rect(this.x, this.y, this.r, this.r);
+    image(coinImg, this.x, this.y, this.r, this.r);
+  }
+
+  move() {
+    this.x += this.speed;
+  }
+}
